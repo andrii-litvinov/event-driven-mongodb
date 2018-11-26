865a141fefb4cfd736c5ba4cbaa88c55a6966190
Andrii Litvinov <andrii.litvinov@gmail.com>
2018-11-26 18:32:50 +0200

    Init demo solution

---
+++ Demo/Service1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Demo/Service2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Service2
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
