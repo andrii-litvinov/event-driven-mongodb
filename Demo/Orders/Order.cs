1aa55ca0b902c40d8dea351163944be55b17d167
Andrii Litvinov <andrii.litvinov@gmail.com>
2018-11-28 19:28:14 +0200

    App paid property

--- Demo/Orders/Order.cs
+++ Demo/Orders/Order.cs
@@ -6,6 +6,7 @@ namespace Orders
     {
         public ObjectId Id { get; set; }
         public decimal TotalAmount { get; set; }
+        public bool Paid { get; set; }
 
         public void Create(decimal totalAmount) => TotalAmount = totalAmount;
     }
